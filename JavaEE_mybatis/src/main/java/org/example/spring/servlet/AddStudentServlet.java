db968592b6fd387f5cb43ab621cc1259158a0625
coach9910 <56526673+coach9910@users.noreply.github.com>
2020-06-18 13:34:58 +0800

    Add files via upload

---
+++ JavaEE_mybatis/src/main/java/org/example/spring/servlet/AddStudentServlet.java
@@ -0,0 +1,31 @@
+package org.example.spring.servlet;
+import org.example.spring.jdbc.StudentJdbc;
+import org.example.spring.model.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/addstudent")
+public class AddStudentServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+
+        try {
+            req.setCharacterEncoding("UTF-8");
+            resp.setContentType("text/html;charset=UTF-8");
+            Student s =new Student();
+            s.setSno(Integer.parseInt(req.getParameter("studentno")));
+            s.setSname(req.getParameter("studentname"));
+            StudentJdbc.addStudent(s);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        resp.setHeader("refresh","5;URL=func.jsp");
+        }
+    }
+
